721e13753e4636620e177597f271bbf060e324e4
台鋼科大_阮文后 <7q13124127@mail.tsust.edu.tw>
2026-01-10 15:07:52 +0800

    Initial Commit

---
+++ ParkingGatePro/ParkingGateProApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkingGateProApp.swift
+//  ParkingGatePro
+//
+//  Created by Hậu Nguyễn on 10/1/26.
+//
+
+import SwiftUI
+
+@main
+struct ParkingGateProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ParkingGateProTests/ParkingGateProTests.swift
@@ -0,0 +1,17 @@
+//
+//  ParkingGateProTests.swift
+//  ParkingGateProTests
+//
+//  Created by Hậu Nguyễn on 10/1/26.
+//
+
+import Testing
+@testable import ParkingGatePro
+
+struct ParkingGateProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
